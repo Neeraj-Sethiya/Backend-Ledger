2cb145818ae14745a0b71a30013eb851d3914881
Neeraj-Sethiya <neerajsethiya21@gmail.com>
2026-02-15 17:50:05 +0530

    added frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001' , 'https://your-frontend-domain.com'], // Add your frontend URLs
+  origin: ['http://localhost:3000', 'http://localhost:3001' , 'https://bank-ledger-six.vercel.app/'], // Add your frontend URLs
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
